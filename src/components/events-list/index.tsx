55d6529782a1d885bc8fef900f4d7df9fc4db091
Kyle Noll <noll.kyle@gmail.com>
2021-07-24 23:06:30 -0700

    Make data events hook that gets any data

--- src/components/events-list/index.tsx
+++ src/components/events-list/index.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import styles from "./index.module.css";
-import { usePayoutClaimedEvent } from "hooks/usePayoutClaimedEvent";
 import { useColonyClient } from "hooks/useColonyClient";
-import { useDomainAddedEvents } from "hooks/useDomainAddedEvents";
-import { useColonyInitialisedEvents } from "hooks/useColonyInitialisedEvents";
-import { useColonyRoleSetEvents } from "hooks/useColonyRoleSetEvents";
+import { useDataEvents } from "hooks/useDataEvents";
 
 export const EventsList: React.FC = () => {
     const colonyClient = useColonyClient();
-    const payoutClaimedEventData = usePayoutClaimedEvent(colonyClient);
-    const domainAddedEvents = useDomainAddedEvents(colonyClient);
-    const colonyInitialisedEvents = useColonyInitialisedEvents(colonyClient);
-    const colonyRoleSetEvents = useColonyRoleSetEvents(colonyClient);
+    const dataEvents = useDataEvents(colonyClient, "DomainAdded");
 
-    console.log(colonyRoleSetEvents);
+    console.log(dataEvents);
 
     return <div className={styles.eventsList}></div>;
 };
---
+++ src/hooks/useDataEvents.ts
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { ColonyClient } from "@colony/colony-js";
+import { Log } from "ethers/providers";
+import { getLogs } from "@colony/colony-js";
+import { EventFilter } from "ethers";
+
+export function useDataEvents(
+    colonyClient: ColonyClient,
+    eventType:
+        | "ColonyInitialised"
+        | "ColonyRoleSet"
+        | "PayoutClaimed"
+        | "DomainAdded"
+) {
+    const [eventLogs, setEventLogs] = React.useState([] as Log[]);
+
+    const filters = React.useMemo(
+        () => colonyClient.filters || ({} as ColonyClient["filters"]),
+        [colonyClient.filters]
+    );
+
+    // Select the eventFilter based on the given eventType
+    const eventFilter = React.useMemo(() => {
+        if (eventType === "ColonyInitialised") {
+            return filters.ColonyInitialised
+                ? filters.ColonyInitialised(null, null)
+                : ({} as EventFilter);
+        } else if (eventType === "ColonyRoleSet") {
+            // I'm adding ts-ignore here because WE know that ColonyRoleSet
+            // exists as a property of filters but Typescript doesn't since
+            // filters can come from one of many different contract versions.
+            //@ts-ignore
+            return filters.ColonyRoleSet
+                ? //@ts-ignore
+                  filters.ColonyRoleSet(null)
+                : ({} as EventFilter);
+        } else if (eventType === "PayoutClaimed") {
+            return filters.PayoutClaimed
+                ? filters.PayoutClaimed(null, null, null)
+                : ({} as EventFilter);
+        } else if (eventType === "DomainAdded") {
+            return filters.DomainAdded
+                ? filters.DomainAdded(null)
+                : ({} as EventFilter);
+        } else {
+            return {} as EventFilter;
+        }
+    }, [filters, eventType]);
+
+    useEffect(() => {
+        if (!colonyClient.provider) return;
+        (async () => {
+            const _eventLogs = await getLogs(colonyClient, eventFilter);
+            setEventLogs(_eventLogs);
+        })();
+    }, [colonyClient, eventFilter]);
+
+    return eventLogs;
+}
